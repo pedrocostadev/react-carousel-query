eb53a34f3e91dfecbed99779db56da79af6a0c7a
Pedro Costa <pedrodanielcc@gmail.com>
2021-01-27 21:54:22 +0000

    Updates build output folder

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   entry: { index: path.resolve(__dirname, 'demo', 'index.js') },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'public'),
   },
   devtool: 'source-map',
   module: {
