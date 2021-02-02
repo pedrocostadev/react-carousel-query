ab1c66018ed3056b9500fe4d07459091eac01c6d
TiagoGomes <t.dfgomes@icloud.com>
2021-02-02 17:35:36 +0000

    [#1]: Fix not centered icon
    Co-authored-by: Tiago Gomes <tiago.gomes@farfetch.com>

--- src/icons/IconChevron.js
+++ src/icons/IconChevron.js
@@ -4,14 +4,15 @@ import PropTypes from 'prop-types';
 const IconChevron = ({ className }) => (
   <svg
     className={className}
-    viewBox="0 0 8 5"
-    height="15px"
-    width="40px"
+    viewBox="-.2 -.3 8 5"
+    height="15"
+    width="40"
     stroke="currentColor"
     fill="none"
     fillRule="evenodd"
     strokeLinecap="round"
     strokeLinejoin="round"
+    preserveAspectRatio="xMidYMax meet"
   >
     <polyline points="1 1 3.77984472 4 6.53846154 1" />
   </svg>
