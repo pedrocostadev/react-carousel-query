34897b88e43bedaf7ca030b9fd7f2bfb9348368b
Pedro Costa <pedro.costa.dev@gmail.com>
2021-02-25 14:53:31 +0000

    Update typescript types

--- index.d.ts
+++ index.d.ts
@@ -4,18 +4,30 @@ declare module 'react-carousel-query' {
     interface Item {
       id: string | number;
     }
-  
-    interface ReactCarouselQueryProps {
-      renderItem: ({ item }: { item: Item }) => React.ReactElement;
-      getData: ({ offset, limit, total }: { offset: number; limit: number, total: number }) => React.ReactElement;
+
+    interface GeneralProps {
       renderBadge?: () => React.ReactElement;
-      fetchStep?: number;
+      renderArrow?: () => React.ReactElement;
       hideIndex?: boolean;
       showArrows?: boolean;
     }
   
+    interface ReactCarouselQueryProps extends GeneralProps {
+      renderItem: ({ item }: { item: Item }) => React.ReactElement;
+      getData: ({ offset, limit, total }: { offset: number; limit: number, total: number }) => React.ReactElement;
+      fetchStep?: number;
+    }
+
+    interface ReactCarouselProps extends GeneralProps {
+      children: React.ReactElement[];
+    }
+  
     const ReactCarouselQuery: React.FC<ReactCarouselQueryProps>;
+
+    const ReactCarousel: React.FC<ReactCarouselProps>;
   
     export default ReactCarouselQuery;
+
+    export { ReactCarousel }
   }
   
