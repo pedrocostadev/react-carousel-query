6ad3718bb6d7abc1bd9ad7d50193add434b183b6
Pedro Costa <pedro.costa.dev@gmail.com>
2021-04-24 22:23:23 +0100

    Feature/storybook (#12)
    Adds storybook

--- src/components/reactCarousel/reactCarousel.js
+++ src/components/reactCarousel/reactCarousel.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import CarouselItemsContainer from '@components/carouselItemsContainer';
-import CarouselItem from '@components/carouselItem';
-import Box from '@primitives/box';
+import CarouselItemsContainer from '@components/carouselItemsContainer'
+import CarouselItem from '@components/carouselItem'
+import Box from '@primitives/box'
 
 const ReactCarousel = ({ children, ...props }) => {
-  const [currentIndex, setCurrentIndex] = React.useState(0);
-  const next = () => setCurrentIndex((currentIndex) => currentIndex + 1);
-  const previous = () => setCurrentIndex((currentIndex) => currentIndex - 1);
-  const total = children.length;
+  const [currentIndex, setCurrentIndex] = React.useState(0)
+  const next = () => setCurrentIndex(currentIndex => currentIndex + 1)
+  const previous = () => setCurrentIndex(currentIndex => currentIndex - 1)
+  const total = children.length
   return (
     <Box positionRelative overflowHidden fullWidth fullHeight>
       <CarouselItemsContainer
@@ -19,16 +19,16 @@ const ReactCarousel = ({ children, ...props }) => {
         total={total}
         {...props}
       >
-        {React.Children.map(children, (child) => (
+        {React.Children.map(children, child => (
           <CarouselItem>{child}</CarouselItem>
         ))}
       </CarouselItemsContainer>
     </Box>
-  );
-};
+  )
+}
 
 ReactCarousel.propTypes = {
   children: PropTypes.arrayOf(PropTypes.node).isRequired,
-};
+}
 
-export default ReactCarousel;
+export default ReactCarousel
--- src/components/reactCarouselQuery/index.js
+++ src/components/reactCarouselQuery/index.js
@@ -12,6 +12,11 @@ const ReactCarouselQueryWrapper = ({ getData, fetchStep, ...props }) => (
 )
 
 ReactCarouselQueryWrapper.propTypes = {
+  renderItem: PropTypes.func.isRequired,
+  renderBadge: PropTypes.func,
+  renderArrow: PropTypes.func,
+  hideIndex: PropTypes.bool,
+  showArrows: PropTypes.bool,
   getData: PropTypes.func.isRequired,
   fetchStep: PropTypes.number,
 }
